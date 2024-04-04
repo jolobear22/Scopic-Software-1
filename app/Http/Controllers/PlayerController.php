324dd58fee96332d13b1520b4a4a5f36c0c2d413
AJ Retuya <117564911+jolobear22@users.noreply.github.com>
2024-04-04 13:40:49 +0800

    create player done

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -5,7 +5,10 @@
 // YOU CAN ADD YOUR CODE TO THIS FILE TO EXTEND THE FEATURES TO USE THEM IN YOUR WORK.
 // /////////////////////////////////////////////////////////////////////////////
 
+
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Player;
 
 class PlayerController extends Controller
 {
@@ -19,9 +22,17 @@ class PlayerController extends Controller
         return response("Failed", 500);
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        return response("Failed", 500);
+        //return response("Success", 200);
+
+        //$users = json_decode($request->all());
+        $player = Player::create([
+            'name' => $request['name'],
+            'position' => $request['position']
+            ]);
+
+        return response()->json($player);
     }
 
     public function update()
