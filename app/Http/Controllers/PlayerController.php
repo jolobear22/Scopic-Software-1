0966c0c55dc39d5aa6394e4d495bb0f31541f260
AJ Retuya <117564911+jolobear22@users.noreply.github.com>
2024-04-04 20:17:27 +0800

    create player with skills

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -9,6 +9,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Player;
+use App\Models\PlayerSkill;
 
 class PlayerController extends Controller
 {
@@ -24,16 +25,44 @@ class PlayerController extends Controller
 
     public function store(Request $request)
     {
-        
+        // Validate the request data
+        $request->validate([
+            'name' => 'required|string',
+            'position' => 'required|string|in:defender,midfielder,forward',
+            'skills' => 'required|array|min:1',
+            'skills.*.skill_name' => 'required|string|in:defense,attack,speed,strength,stamina',
+            'skills.*.value' => 'required|integer|min:0|max:100',
+        ]);
+
+        // Create the player
+        $player = Player::create([
+            'name' => $request->name,
+            'position' => $request->position,
+        ]);
+
+        // Attach skills to the player
+        foreach ($request->skills as $skillData) {
+            $skill = new PlayerSkill([
+                'skill_name' => $skillData['skill_name'],
+                'value' => $skillData['value'],
+            ]);
+            $player->skills()->save($skill);
+        }
+
+        // Fetch the player with their skills
+        $playerWithSkills = Player::with('skills')->find($player->id);
+
+        return response()->json($playerWithSkills, 201);
+
 
         //$users = json_decode($request->all());
-        $player = Player::create([
-            'name' => $request['name'],
-            'position' => $request['position']
-            ]);
+        // $player = Player::create([
+        //     'name' => $request['name'],
+        //     'position' => $request['position']
+        //     ]);
 
-        return response()->json($player);
-        return response("", 200);
+        // return response()->json($player);
+        // return response("", 200);
     }
 
     public function update()
